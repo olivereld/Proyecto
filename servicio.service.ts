480c29833c7488b341b690c9c6114584d9277cc9
olivereld <programadorctg@gmail.com>
2022-08-04 15:03:40 -0400

    algo

--- servicio.service.ts
+++ servicio.service.ts
@@ -1,7 +1,8 @@
 class Servicio {
   nombre:string;
   apellido:string
-  constructor(nombre:string,apellido:string){
+  notas:string;
+  constructor(nombre:string,apellido:string,notas:string){
     this.nombre=nombre;
     this.apellido=apellido;
   }
